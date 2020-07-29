2c4971a0a1c1ab315a97390a0fd6dc3b664010e7
Laura Kenny <l.j.kenny@hotmail.com>
2020-07-29 16:00:26 +0100

    Implements redux-persist to use local storage.

--- index.js
+++ index.js
@@ -4,18 +4,21 @@
 
 import 'react-native-gesture-handler';
 import React from 'react';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import configureStore from './src/store/configureStore';
 
-const store = configureStore()
+const { store, persistor } = configureStore()
 
 const RNRedux = () => (
   <Provider store = { store }>
-    <App />
+    <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 )
 
--- src/store/configureStore.tsx
+++ src/store/configureStore.tsx
@@ -1,12 +1,23 @@
 import { createStore, combineReducers } from 'redux';
 import roomReducer from './rooms/roomReducer';
+import { persistStore, persistReducer } from 'redux-persist'
+import AsyncStorage from '@react-native-community/async-storage';
 
 const rootReducer = combineReducers({ 
     rooms: roomReducer,
 });
 
-const configureStore = () => {
-    return createStore(rootReducer);
-}
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+  }
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const configureStore = () => {
+    const store = createStore(persistedReducer);
+    const persistor = persistStore(store);
+    return { persistor, store };
+  };
+  
 export default configureStore;
