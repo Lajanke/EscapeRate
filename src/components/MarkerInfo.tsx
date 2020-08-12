b76447879f6cdbb713793bfdd3b95c3e01fb139a
Laura Kenny <l.j.kenny@hotmail.com>
2020-08-12 16:03:03 +0100

    component to display marker info.

---
+++ src/components/MarkerInfo.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet, Modal } from 'react-native';
+
+export interface MarkerInfoProps {
+    company: any
+}
+
+const MarkerInfo: React.FC<MarkerInfoProps> = (props) => {
+  return (
+      
+    <View style={styles.container}>
+        <Text>{props.company.name}</Text>
+        <Text>{props.company.website}</Text>
+    </View>
+    
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        width: 300,
+        height: 50,
+        position: "absolute",
+        bottom: 0,
+    }
+  });
+
+export default MarkerInfo;
