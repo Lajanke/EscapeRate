3716676c039bef516fa27b633e4fcb641350c62f
Laura Kenny <l.j.kenny@hotmail.com>
2020-08-13 15:22:59 +0100

    adds close button and scrollview.

--- src/components/AddRoomForm.tsx
+++ src/components/AddRoomForm.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Button, TextInput, View, Text, Image, DatePickerAndroid, DatePickerIOSBase, Platform } from 'react-native';
+import { StyleSheet, Button, TextInput, View, Text, Image, DatePickerAndroid, DatePickerIOSBase, Platform, SafeAreaView } from 'react-native';
 import { CheckBox } from 'react-native-elements';
 import { Formik, Field } from 'formik';
 import { Room } from './HomeScreen';
@@ -8,7 +8,7 @@ import * as yup from 'yup';
 import ImagePicker from 'react-native-image-picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { TouchableOpacity, TouchableHighlight } from 'react-native-gesture-handler';
+import { TouchableOpacity, TouchableHighlight, ScrollView } from 'react-native-gesture-handler';
 
 const newRoomSchema = yup.object({
     name: yup.string().required('Required'),
@@ -55,137 +55,144 @@ const AddRoomForm: React.FC<AddRoomFormProps> = (props) => {
     const [show, setShow] = useState(false);
     const [uploadImage, setImage] = useState(false);
 
-    return <View>
-        <Formik
-            initialValues={{
-                name: '',
-                date: new Date(),
-                escaped: false,
-                timeLimit: '',
-                time: '',
-                groupSize: '',
-                image: '',
-                company: '',
-            }}
-            validationSchema={newRoomSchema}
-            validateOnChange={true}
-            onSubmit={(values) => {
-                addRoom(values)
-            }}
-        >
-            {(formikProps) => (
-                <View style={styles.formContainer}>
-                    <TextInput placeholder='Room name' 
-                    onChangeText={formikProps.handleChange('name')}
-                    value={formikProps.values.name}
-                    style={styles.input}
-                    />
-                    {formikProps.touched.name && formikProps.errors.name &&
-                        <Text style={{ fontSize: 10, color: 'red' }}>{formikProps.errors.name}</Text>
-                    }              
-                
-                    <TextInput placeholder='Group Size' 
-                    onChangeText={formikProps.handleChange('groupSize')}
-                    value={formikProps.values.groupSize}
-                    keyboardType={"numeric"}
-                    style={styles.input}
-                    />
-                    {formikProps.touched.groupSize && formikProps.errors.groupSize &&
-                        <Text style={{ fontSize: 10, color: 'red' }}>{formikProps.errors.groupSize}</Text>
-                    }
-
-                    <TextInput placeholder='Time Limit' 
-                    onChangeText={formikProps.handleChange('timeLimit')}
-                    value={formikProps.values.timeLimit}
-                    keyboardType={"numeric"}
-                    style={styles.input}
-                    />
-                    {formikProps.touched.timeLimit && formikProps.errors.timeLimit &&
-                        <Text style={{ fontSize: 10, color: 'red' }}>{formikProps.errors.timeLimit}</Text>
-                    }
-
-                    <TextInput placeholder='How long did it take (minutes)' 
-                    onChangeText={formikProps.handleChange('time')}
-                    value={formikProps.values.time}
-                    keyboardType={"numeric"}
-                    style={styles.input}
-                    />
-                    {formikProps.touched.time && formikProps.errors.time &&
-                        <Text style={{ fontSize: 10, color: 'red' }}>{formikProps.errors.time}</Text>
-                    }
-
-                    <View style={styles.checkboxRow}>
-                        <Text style={styles.checkboxText}>Did You Escape?</Text>
-                    <CheckBox
-                        checked={formikProps.values.escaped}
-                        iconType='material-community'
-                        checkedIcon='lock-open-variant-outline'
-                        uncheckedIcon='lock-outline'
-                        onPress={() => {
-                            formikProps.setFieldValue('escaped', !formikProps.values.escaped)}
-                        }
-                        checkedColor='green'
-                        uncheckedColor='red'
-                        iconRight={true}
-                    />
-                    </View>
-
-                    <View style={styles.calendar}>
-                        <Text style={{marginTop: 7, marginLeft: 3}}>Date Visited: {new Date(formikProps.values.date).toDateString()}</Text>  
-                        <Icon name='calendar' size={24} style={{marginRight: 22}} onPress={() => {
-                        setShow(true);
-                        }}/>
-                    </View> 
-
-                    {show && (
-                        <View>
-                            <DateTimePicker
-                                testID="dateTimePicker"
-                                value={formikProps.values.date} 
-                                display="default"
-                                maximumDate={new Date()}
-                                onChange={(event, selectedDate) => {
-                                    setShow(false);
-                                    // update formik last as this will cause a re-render
-                                    formikProps.setFieldValue('date', selectedDate || formikProps.values.date);
-                                }}
+    return <SafeAreaView>
+        <ScrollView>
+            <View>
+                <Formik
+                    initialValues={{
+                        name: '',
+                        date: new Date(),
+                        escaped: false,
+                        timeLimit: '',
+                        time: '',
+                        groupSize: '',
+                        image: '',
+                        company: '',
+                    }}
+                    validationSchema={newRoomSchema}
+                    validateOnChange={true}
+                    onSubmit={(values) => {
+                        addRoom(values)
+                    }}
+                >
+                    {(formikProps) => (
+                        <View style={styles.formContainer}>
+                            <TextInput placeholder='Room name' 
+                            onChangeText={formikProps.handleChange('name')}
+                            value={formikProps.values.name}
+                            style={styles.input}
                             />
-                        </View>
-                    )}
+                            {formikProps.touched.name && formikProps.errors.name &&
+                                <Text style={{ fontSize: 10, color: 'red' }}>{formikProps.errors.name}</Text>
+                            }              
+                        
+                            <TextInput placeholder='Group Size' 
+                            onChangeText={formikProps.handleChange('groupSize')}
+                            value={formikProps.values.groupSize}
+                            keyboardType={"numeric"}
+                            style={styles.input}
+                            />
+                            {formikProps.touched.groupSize && formikProps.errors.groupSize &&
+                                <Text style={{ fontSize: 10, color: 'red' }}>{formikProps.errors.groupSize}</Text>
+                            }
 
-                    <TextInput placeholder='Company' 
-                    onChangeText={formikProps.handleChange('company')}
-                    value={formikProps.values.company}
-                    style={styles.input}
-                    />
-                    <View style={styles.imageUploadButton}>
-                        <Button
-                            title='Upload Image'
-                            onPress={() => {
-                                const options = {
-                                    noData: true
-                                };
-                            ImagePicker.launchImageLibrary(options, (response) => {
-                                if (response.uri) {
-                                    formikProps.values.image = `${response.uri}`
-                                    setImage(true)
+                            <TextInput placeholder='Time Limit' 
+                            onChangeText={formikProps.handleChange('timeLimit')}
+                            value={formikProps.values.timeLimit}
+                            keyboardType={"numeric"}
+                            style={styles.input}
+                            />
+                            {formikProps.touched.timeLimit && formikProps.errors.timeLimit &&
+                                <Text style={{ fontSize: 10, color: 'red' }}>{formikProps.errors.timeLimit}</Text>
+                            }
+
+                            <TextInput placeholder='How long did it take (minutes)' 
+                            onChangeText={formikProps.handleChange('time')}
+                            value={formikProps.values.time}
+                            keyboardType={"numeric"}
+                            style={styles.input}
+                            />
+                            {formikProps.touched.time && formikProps.errors.time &&
+                                <Text style={{ fontSize: 10, color: 'red' }}>{formikProps.errors.time}</Text>
+                            }
+
+                            <View style={styles.checkboxRow}>
+                                <Text style={styles.checkboxText}>Did You Escape?</Text>
+                            <CheckBox
+                                checked={formikProps.values.escaped}
+                                iconType='material-community'
+                                checkedIcon='lock-open-variant-outline'
+                                uncheckedIcon='lock-outline'
+                                onPress={() => {
+                                    formikProps.setFieldValue('escaped', !formikProps.values.escaped)}
                                 }
-                            });
-                            }}
-                        >
-                    </Button>
-                    {uploadImage && (
-                        <Image source={{uri: formikProps.values.image}} style={{height: 100, resizeMode: 'contain', marginTop: 24}}/>
+                                checkedColor='green'
+                                uncheckedColor='red'
+                                iconRight={true}
+                            />
+                            </View>
+
+                            <View style={styles.calendar}>
+                                <Text style={{marginTop: 7, marginLeft: 3}}>Date Visited: {new Date(formikProps.values.date).toDateString()}</Text>  
+                                <Icon name='calendar' size={24} style={{marginRight: 22}} onPress={() => {
+                                setShow(true);
+                                }}/>
+                            </View> 
+
+                            {show && (
+                                <View>
+                                    <DateTimePicker
+                                        testID="dateTimePicker"
+                                        value={formikProps.values.date} 
+                                        display="default"
+                                        maximumDate={new Date()}
+                                        onChange={(event, selectedDate) => {
+                                            setShow(false);
+                                            // update formik last as this will cause a re-render
+                                            formikProps.setFieldValue('date', selectedDate || formikProps.values.date);
+                                        }}
+                                    />
+                                </View>
+                            )}
+
+                            <TextInput placeholder='Company' 
+                            onChangeText={formikProps.handleChange('company')}
+                            value={formikProps.values.company}
+                            style={styles.input}
+                            />
+                            <View style={styles.imageUploadButton}>
+                                <Button
+                                    title='Upload Image'
+                                    onPress={() => {
+                                        const options = {
+                                            noData: true
+                                        };
+                                    ImagePicker.launchImageLibrary(options, (response) => {
+                                        if (response.uri) {
+                                            formikProps.values.image = `${response.uri}`
+                                            setImage(true)
+                                        }
+                                    });
+                                    }}
+                                >
+                            </Button>
+                            {uploadImage && (
+                                <Image source={{uri: formikProps.values.image}} style={{height: 100, resizeMode: 'contain', marginTop: 24}}/>
+                            )}
+                            <View style={styles.submitButton}>
+                            <Button title='submit' onPress={formikProps.handleSubmit} color='#536e96' />
+                            </View>
+                            
+                            </View>
+                        </View>   
                     )}
-                    <View style={styles.submitButton}>
-                    <Button title='submit' onPress={formikProps.handleSubmit} color='#536e96' />
-                    </View>
-                    
-                    </View>
-                </View>   
-            )}
-        </Formik>
-    </View>
+                </Formik>
+                <View style={styles.closeButton}>
+          <Button title='Close' onPress={() => props.setModalState(false)} color='orange' />
+          </View>
+            </View>
+        </ScrollView>
+    </SafeAreaView> 
 }
 
 const styles = StyleSheet.create({
@@ -216,7 +223,11 @@ const styles = StyleSheet.create({
     calendar: {
        flexDirection: 'row',
        justifyContent: 'space-between' 
-    }
+    },
+    closeButton: {
+        marginHorizontal: 40,
+        marginBottom: 24,
+      }
   });
   
 
