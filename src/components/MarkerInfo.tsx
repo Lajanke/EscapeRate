31b147690343771c4c6dd03b8d7e5b6831bb6c8e
Laura Kenny <l.j.kenny@hotmail.com>
2020-08-14 00:43:12 +0100

    links marker to website.

--- src/components/MarkerInfo.tsx
+++ src/components/MarkerInfo.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet, Modal } from 'react-native';
+import { View, Text, StyleSheet, Linking } from 'react-native';
 
 export interface MarkerInfoProps {
     company: any
 }
 
 const MarkerInfo: React.FC<MarkerInfoProps> = (props) => {
-  return (
-      
+  return (  
     <View style={styles.container}>
-        <Text>{props.company.name}</Text>
-        <Text>{props.company.website ? props.company.website : 'No Website Info'}</Text>
+        <Text style={styles.name}>{props.company.name}</Text>
+        {props.company.website && (
+          <Text style={styles.website} onPress={() => Linking.openURL(props.company.website)}>Website Link</Text>
+        )}
+        {!props.company.website && (
+          <Text style={styles.website}>No Website Info</Text>
+        )}
     </View>
     
   );
@@ -18,11 +22,30 @@ const MarkerInfo: React.FC<MarkerInfoProps> = (props) => {
  
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#fff',
+        backgroundColor: '#edfcfc',
         width: 300,
-        height: 50,
+        height: 110,
         position: "absolute",
         bottom: 0,
+        borderTopRightRadius: 20,
+        borderColor: '#384963',
+        borderWidth: 1,
+    },
+    name: {
+      fontSize: 16, 
+      padding: 10,
+      alignSelf: 'center',
+    },
+    website: {
+      color: '#fff',
+      alignSelf: 'center',
+      backgroundColor: '#384963',
+      paddingVertical: 5,
+      paddingHorizontal: 10,
+      borderRadius: 5,
+      position: 'absolute',
+      bottom: 16,
+      alignItems: 'center'
     }
   });
 
